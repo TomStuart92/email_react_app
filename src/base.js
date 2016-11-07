ec7ab11eac81e768b236e0c21d701d3e0c1b6829
TomStuart92 <Thomascstuart@gmail.com>
2016-11-07 11:13:56 +0000

    Credentials

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBvUtCvAFrf1TpE_8ZWrR4bN_pGKUs3qzk",
+    authDomain: "emailclient-2dc0a.firebaseapp.com",
+    databaseURL: "https://emailclient-2dc0a.firebaseio.com",
+});
+
+export default base;
